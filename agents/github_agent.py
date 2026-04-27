b8d2285a2b9f34847b8a775403def62e70dd2393
sandya kandregula <52678059+sandy2341@users.noreply.github.com>
2026-04-27 23:36:16 +0530

    Add function to push project to GitHub

--- agents/github_agent.py
+++ agents/github_agent.py
@@ -1,1 +1,9 @@
+from git import Repo
 
+def push_to_github(project_name):
+    repo = Repo.init(project_name)
+    repo.git.add(all=True)
+    repo.index.commit("Initial commit")
+
+    repo.create_remote("origin", "https://github.com/YOUR_USERNAME/YOUR_REPO.git")
+    repo.git.push("--set-upstream", "origin", "main")
